4dba7863732403abaa05eb3cbdf2d727c2df17cc
willy <w.lunah@protonmail.com>
2023-05-22 06:05:21 -0400

    agregado react

--- core/settings.py
+++ core/settings.py
@@ -142,7 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'es'
-TIME_ZONE = 'UTC-4'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
